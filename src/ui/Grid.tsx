460f52b783d439dc285647dc5900e8b89235dea4
Pedro Maia Frigo <frigopedro@icloud.com>
2023-03-29 13:53:27 -0300

    first commit

---
+++ src/ui/Grid.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+interface GridProps {
+  children?: React.ReactNode;
+}
+export default function Grid({ children }: GridProps) {
+  return <div className="w-full p-2 max-w-screen-xl m-auto">{children}</div>;
+}
